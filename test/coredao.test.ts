88add1f3a54a41c9e97a1c0f9d5d1ae94981f339
ryuhei <ryuheimat3@gmail.com>
2021-09-23 11:15:28 +0900

    add MockToken and treasury test

--- test/coredao.test.ts
+++ test/coredao.test.ts
@@ -3,21 +3,22 @@ import { Signer } from "ethers"
 import { ethers, deployments } from "hardhat"
 import { CoreDAO } from "../types"
 
-let coredao: CoreDAO
-let accounts: Signer[]
-let treasury: Signer
-const startingCoreDAOAmount = ethers.utils.parseEther("1000")
-const NAME = "CORE DAO"
-const SYMBOL = "coreDAO"
-
 describe("CoreDAO", function () {
-  beforeEach(async function () {
-    await deployments.fixture()
+  let treasury: Signer
+  let alice: Signer
+  let bob: Signer
+  let coredao: CoreDAO
+  const startingCoreDAOAmount = ethers.utils.parseEther("1000")
+  const NAME = "CORE DAO"
+  const SYMBOL = "coreDAO"
 
-    accounts = await ethers.getSigners()
+  beforeEach(async function () {
+    const accounts = await ethers.getSigners()
     treasury = accounts[0]
-    const coreDaoFactory = await ethers.getContractFactory("CoreDAO")
-    coredao = (await coreDaoFactory.deploy(startingCoreDAOAmount, await treasury.getAddress())) as CoreDAO
+    alice = accounts[1]
+    bob = accounts[2]
+    const CoreDaoFactory = await ethers.getContractFactory("CoreDAO")
+    coredao = (await CoreDaoFactory.deploy(startingCoreDAOAmount, await treasury.getAddress())) as CoreDAO
   })
 
   describe("check initial state", () => {
@@ -37,13 +38,13 @@ describe("CoreDAO", function () {
 
   describe("#issue function", () => {
     it("revert if msg.sender is not treasury", async () => {
-      expect(coredao.connect(accounts[1]).issue("1", await accounts[2].getAddress())).to.revertedWith("Not treasury")
+      expect(coredao.connect(alice).issue("1", await bob.getAddress())).to.be.revertedWith("Not treasury")
     })
 
     it("should issue DAO token by treasury", async () => {
       const amount = ethers.utils.parseEther("10")
-      await coredao.connect(treasury).issue(amount, await accounts[1].getAddress())
-      expect(await coredao.balanceOf(await accounts[1].getAddress())).to.equal(amount)
+      await coredao.connect(treasury).issue(amount, await alice.getAddress())
+      expect(await coredao.balanceOf(await alice.getAddress())).to.equal(amount)
     })
   })
 })
--- test/treasury.test.ts
+++ test/treasury.test.ts
@@ -1,19 +1,78 @@
 import { expect } from "chai"
-import { BigNumber } from "ethers"
-import { ethers, deployments } from "hardhat"
-import { CoreDAOTreasury } from "../types"
-import { blockNumber, getBigNumber, impersonate } from "./utilities"
-
-let treasury: CoreDAOTreasury
+import { Signer, utils, constants } from "ethers"
+import { ethers, upgrades } from "hardhat"
+import { CoreDAOTreasury, CoreDAO } from "../types"
 
 describe("CoreDAOTreasury", function () {
-  beforeEach(async function () {
-    await deployments.fixture()
+  let owner: Signer
+  let alice: Signer
+  let bob: Signer
+  let treasury: CoreDAOTreasury
+  let coredao: CoreDAO
+  const startingCoreDAOAmount = utils.parseEther("1000")
 
-    treasury = await ethers.getContract("CoreDAOTreasury")
+  beforeEach(async function () {
+    const accounts = await ethers.getSigners()
+    owner = accounts[0]
+    alice = accounts[1]
+    bob = accounts[2]
+
+    const CoreDAOTreasuryFactory = await ethers.getContractFactory("CoreDAOTreasury")
+    treasury = (await upgrades.deployProxy(CoreDAOTreasuryFactory, [])) as CoreDAOTreasury
+
+    const CoreDaoFactory = await ethers.getContractFactory("CoreDAO")
+    coredao = (await CoreDaoFactory.deploy(startingCoreDAOAmount, treasury.address)) as CoreDAO
   })
 
-  it("should not be able to mint", async () => {
-    const [deployer, account1] = await ethers.getSigners()
+  describe("check initial state", async () => {
+    it("check owner", async () => {
+      expect(await treasury.owner()).to.equal(await owner.getAddress())
+    })
+
+    // it("check coredao", async () => {
+    //   expect(await treasury.coreDAO()).to.equal(coredao.address)
+    // })
+  })
+
+  describe("#pay", async () => {
+    const payAmount = utils.parseEther("1")
+
+    it("revert if msg.sender is not owner", async () => {
+      await expect(treasury.connect(alice).pay(await alice.getAddress(), payAmount, constants.AddressZero, "test")).to.be.revertedWith(
+        "Ownable: caller is not the owner"
+      )
+    })
+
+    it.only("pay ether", async () => {
+      const treasuryBalance = utils.parseEther("10")
+      await owner.sendTransaction({
+        to: treasury.address,
+        value: treasuryBalance,
+      })
+
+      const aliceBalanceBefore = await alice.getBalance()
+      const tx = await treasury.connect(owner).pay(await alice.getAddress(), payAmount, constants.AddressZero, "test")
+      expect(tx)
+        .to.emit(treasury, "Payment")
+        .withArgs(await alice.getAddress(), constants.AddressZero, payAmount, "test")
+
+      expect(await owner.provider!.getBalance(treasury.address)).to.equal(treasuryBalance.sub(payAmount))
+      expect(await alice.getBalance()).to.equal(aliceBalanceBefore.add(payAmount))
+    })
+
+    it("pay ERC20 token", async () => {
+      const MockTokenFactory = await ethers.getContractFactory("MockToken")
+      const mockToken = await MockTokenFactory.deploy()
+      const treasuryBalance = utils.parseEther("100")
+      await mockToken.connect(owner).transfer(treasury.address, treasuryBalance)
+
+      const tx = await treasury.connect(owner).pay(await alice.getAddress(), payAmount, mockToken.address, "test")
+      expect(tx)
+        .to.emit(treasury, "Payment")
+        .withArgs(await alice.getAddress(), mockToken.address, payAmount, "test")
+
+      expect(await mockToken.balanceOf(treasury.address)).to.equal(treasuryBalance.sub(payAmount))
+      expect(await mockToken.balanceOf(await alice.getAddress())).to.equal(payAmount)
+    })
   })
 })
