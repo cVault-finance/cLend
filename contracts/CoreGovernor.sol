f53d1fa6c1b7d61e815fc989f508bb1a4f3bc5f3
notsegfault <notsegfault@gmail.com>
2022-01-06 21:05:14 -0500

    wrong name

--- contracts/CoreGovernor.sol
+++ contracts/CoreGovernor.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
 
-contract MyGovernor is
+contract CoreGovernor is
     Governor,
     GovernorSettings,
     GovernorCountingSimple,
