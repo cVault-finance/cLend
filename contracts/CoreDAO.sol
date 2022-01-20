1895adffdc6b3ee2884864755def2adc892698d7
x3 <x3dev@protonmail.com>
2022-01-20 14:32:45 -0600

    more mainnet tweaks

--- contracts/CoreDAO.sol
+++ contracts/CoreDAO.sol
@@ -10,11 +10,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract CoreDAO is ERC20 {
     address public constant CORE_DAO_TREASURY = 0xe508a37101FCe81AB412626eE5F1A648244380de;
 
-    /// @notice
-    /// controller is initially the cLending contract, this can be modified
-    /// by upgrading the cLending contract via the Governor contract voting with this tokens
-    constructor(uint256 startingCOREDAOAmount) ERC20("CORE DAO", "CoreDAO") {
-        _mint(CORE_DAO_TREASURY, startingCOREDAOAmount);
+    constructor() ERC20("CORE DAO", "CoreDAO") {
     }
 
     function issue(address to, uint256 amount) public {
