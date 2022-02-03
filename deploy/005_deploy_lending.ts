96631213dd72a72d2b6960dec7bcff07f8f00d9b
notsegfault <notsegfault@gmail.com>
2022-02-03 16:57:10 -0500

    change unit test to upgrade from mainnet proxy

--- deploy/005_deploy_lending.ts
+++ deploy/005_deploy_lending.ts
@@ -19,7 +19,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   await impersonate(DEPLOYER)
   const cvaultDeployer = await ethers.getSigner(DEPLOYER)
 
-  await deploy("CLending", {
+ /* await deploy("CLending", {
     from: deployer,
     args: [],
     log: true,
@@ -31,16 +31,15 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   })
 
   const CLending = await ethers.getContract<CLending>("CLending")
+*/
 
-  /*
   const CLending = await ethers.getContractAt<CLending>("CLending", "0x54B276C8a484eBF2a244D933AF5FFaf595ea58c5")
   await deployments.save("CLending", {
     abi: require("../abi/CLending.json"),
     address: CLending.address,
   })
-  */
 
-  /*
+  
   ///// Example of upgrading
   await deploy("CLendingImplementationFix", {
     from: deployer,
@@ -53,7 +52,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const TeamProxy = await ethers.getContractAt<MockProxyAdmin>("MockProxyAdmin", "0x9cb1eEcCd165090a4a091209E8c3a353954B1f0f")
   await TeamProxy.connect(cvaultDeployer).upgrade(CLending.address, CLendingImplementationFix.address)
   //////
-  */
 
   const CoreDAO = await ethers.getContract("CoreDAO")
   const CORE = await ethers.getContractAt<CORE>("CORE", constants.CORE)
